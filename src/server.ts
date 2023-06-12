3453d840d39be82d392f136fe23cb6a78633944e
Ziad Aboalmjd <ziadgamalaboalmajd@gmail.com>
2023-06-12 17:33:46 +0300

    main

--- src/server.ts
+++ src/server.ts
@@ -17,6 +17,7 @@ const app: express.Application = express();
 const address: string = "0.0.0.:3000";
 
 const corsOption = {
+  // origin: 'https://ziadaboalmajd.github.io',
   origin: 'http://localhost:3000',
   credentials: true,
   optionSuccessStatus: 200,
